3649d3d501f3e4494c5f0fc73f8a64110315bd6f
fabriciosantana <fabricio.santana@gmail.com>
2021-08-17 08:47:54 -0300

    transactions test

--- test/Token.js
+++ test/Token.js
@@ -22,7 +22,7 @@ describe("Token contract", function () {
     console.log(await hardhatToken.totalSupply());
     
     console.log(await hardhatToken.balanceOf(owner.address));
-    
+
     const ownerBalance = await hardhatToken.balanceOf(owner.address);
 
     console.log(await hardhatToken.balanceOf(owner.address));
@@ -30,3 +30,21 @@ describe("Token contract", function () {
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
   });
 });
+
+describe("Transactions", function () {
+    it("Should transfer tokens between accounts", async function () {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+  
+      const Token = await ethers.getContractFactory("Token");
+  
+      const hardhatToken = await Token.deploy();
+  
+      // Transfer 50 tokens from owner to addr1
+      await hardhatToken.transfer(addr1.address, 50);
+      expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+  
+      // Transfer 50 tokens from addr1 to addr2
+      await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+      expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
+    });
+  });
