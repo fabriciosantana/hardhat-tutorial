036c098598f027a65737a71516d4912b1764a983
fabriciosantana <fabricio.santana@gmail.com>
2021-08-21 13:39:11 -0300

    dapp development started

---
+++ frontend/src/components/Dapp.js
@@ -0,0 +1,42 @@
+import React from "react";
+//import logo from './logo.svg';
+//import './App.css';
+
+// We'll use ethers to interact with the Ethereum network and our contract
+import { ethers } from "ethers";
+
+// We import the contract's artifacts and address here, as we are going to be
+// using them with ethers
+import TokenArtifact from "../contracts/Token.json";
+import contratAddress from "../contracts/contract-address.json";
+import { render } from "react-dom";
+
+// This is the Hardhat Network id, you might change it in the hardhat.config.js
+// Here's a list of network ids https://docs.metamask.io/guide/ethereum-provider.html#properties
+// to use when deploying to other networks.
+const HARDHAT_NETWORK_ID = '31337';
+
+// This is an error code that indicates that the user canceled a transaction
+const ERROR_CODE_TX_REJECTED_BY_USER = 4001;
+
+// This component is in charge of doing these things:
+//   1. It connects to the user's wallet
+//   2. Initializes ethers and the Token contract
+//   3. Polls the user balance to keep it updated.
+//   4. Transfers tokens by sending transactions
+//   5. Renders the whole application
+//
+// Note that (3) and (4) are specific of this sample application, but they show
+// you how to keep your Dapp and contract's state in sync,  and how to send a
+// transaction.
+export class Dapp extends React.Component{
+  constructor(props){
+    super(props);
+  };
+
+  render(){
+    return (<div>Hello World!</div>);
+  }
+
+};
+
