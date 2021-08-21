736d10e9c5f4bf0fb5b26e16654d041e18fd5c49
fabriciosantana <fabricio.santana@gmail.com>
2021-08-21 16:41:58 -0300

    ConnectWallet done. NetworkErrorMessage added and done

--- frontend/src/components/ConnectWallet.js
+++ frontend/src/components/ConnectWallet.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { NetworkErrorMessage } from "./NetworkErrorMessage";
 
-export function ConnectWallet(){
-    return (<div>Hello World from component ConnectWallet! You have an Ethereum Wallet</div>);
-}
+export function ConnectWallet({ connectWallet, networtError, dismiss}){
+    return (
+        <div className="container">
+            <div className="row justify-content-md-center">
+                <div className="col-12 text-center">
+                    {/* Metamask network should be set to Localhost:8545. */}
+                    {networtError && (
+                        <NetworkErrorMessage
+                            message={networtError}
+                            dismiss={dismiss}/>
+                    )}
+                </div>
+                <div className="col-6 p-4 text-center">
+                    <p>Please connect to your wallet.</p>
+                    <button
+                        className="btn btn-warning"
+                        type="button"
+                        onClick={connectWallet}
+                    >
+                        Connect Wallet
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
